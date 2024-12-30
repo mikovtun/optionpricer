b1a6403872ef508e017fd8a7beec1e62fc2131c9
Mikael <mikovtun@uw.edu>
2024-12-30 01:53:19 -0800

    Expose classes to python

--- src/python.cxx
+++ src/python.cxx
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <string>
+#include <map>
 #include "stock.h"
 #include "option.h"
 
@@ -6,6 +8,70 @@
 namespace py = pybind11;
 
 namespace OP {
+  
+ const std::map<Device, std::string> DeviceNames = {
+  {Device::cpu, "_cpu"},
+  {Device::gpu, "_gpu"}
+};
 
+// Templated helper function to expose LogNormalStock
+template <Device d>
+void expose_LogNormalStock(py::module_ &m) {
+    std::string class_name = std::string("LogNormalStock") + DeviceNames.at(d);
+    py::class_<LogNormalStock<d>, Stock>(m, class_name.c_str())
+        .def(py::init<float, float, float>())  
+        .def("getPrices", &LogNormalStock<d>::getPrices)  
+        .def_readwrite("bias", &LogNormalStock<d>::bias)  
+        .def_readwrite("volatility", &LogNormalStock<d>::volatility);  
+}
+
+// Templated helper function to expose LogNormalStockDividend
+template <Device d>
+void expose_LogNormalStockDividend(py::module_ &m) {
+    std::string class_name = std::string("LogNormalStockDividend") + DeviceNames.at(d);
+    py::class_<LogNormalStockDividend<d>, Stock>(m, class_name.c_str())
+        .def(py::init<float, float, float, float>())  
+        .def("getPrices", &LogNormalStockDividend<d>::getPrices)  
+				.def_readwrite("dividendRate", &LogNormalStockDividend<d>::dividendRate);
+}
+
+// Templated helper function to expose LogNormalStockDiscreteDividend
+template <Device d>
+void expose_LogNormalStockDiscreteDividend(py::module_ &m) {
+    std::string class_name = std::string("LogNormalStockDiscreteDividend") + DeviceNames.at(d);
+    py::class_<LogNormalStockDiscreteDividend<d>, Stock>(m, class_name.c_str())
+        .def(py::init<float, float, float, float, float, float>())  
+        .def("getPrices", &LogNormalStockDiscreteDividend<d>::getPrices)  
+				.def_readwrite("dividendRate", &LogNormalStockDiscreteDividend<d>::dividendRate)
+				.def_readwrite("daysTillFirstDividend", &LogNormalStockDiscreteDividend<d>::daysTillFirstDividend)
+				.def_readwrite("dividendInterval", &LogNormalStockDiscreteDividend<d>::dividendInterval);
+}
+
+
+
+
+
+
+
+PYBIND11_MODULE(optionpricer, m) {
+  py::enum_<Device>(m, "Device")
+    .value("cpu", Device::cpu)
+    .value("gpu", Device::gpu)
+    .export_values();
+
+
+  py::class_<Stock>(m, "Stock")
+    .def("getPrices", &Stock::getPrices)
+    .def_readwrite("start", &Stock::start);
+
+	expose_LogNormalStock<Device::cpu>(m);
+	expose_LogNormalStock<Device::gpu>(m);
+
+	expose_LogNormalStockDividend<Device::cpu>(m);
+	expose_LogNormalStockDividend<Device::gpu>(m);
+
+	expose_LogNormalStockDiscreteDividend<Device::cpu>(m);
+	expose_LogNormalStockDiscreteDividend<Device::gpu>(m);
+}
 
 }
